1c8ef5ecf8f6babb475a77bf85e3192f56495115
Annafi <annafi.rafy10@gmail.com>
2025-11-29 13:50:49 +0700

    Update Header Composer

--- composer.h
+++ composer.h
@@ -22,4 +22,16 @@ struct listCom {
     adrCom last;
 };
 
+void createList(listCom &L);
+adrCom createComposer(string nama, int tahun, string genre, string negara);
+void insertFirst(listCom &L, adrCom p);
+void insertLast(listCom &L, adrCom p);
+void insertAfter(listCom &L, adrCom prec, adrCom p);
+void viewComNegara(listCom L, string negara);
+void deleteFirst(listCom &L, adrCom &p);
+void deleteLast(listCom &L, adrCom &p);
+void deleteAfter(listCom &L, adrCom q, adrCom &p);
+adrCom findComp(listCom L, string namaComp);
+void deleteComp(listCom &L, string namaComp);
+void viewCompGenre(listCom L, string genre);
 #endif // COMPOSER_H_INCLUDED
